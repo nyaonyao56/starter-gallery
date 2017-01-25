70f51c7c9898bbf400324619367569ae8b045609
Hayden <Hayden@142.232.161.217>
2017-01-24 20:02:38 -0800

    Added _cell.php for internal use Signed-off-by:Hayden <Hayden@142.232.161.217>

---
+++ application/views/_cell.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<a href="./data/{filename}" data-lightbox="galery" data-title="{title}...Posted{uploaded}, in {catagory}">
+    <img src="./data/thumb{filename}">
+</a>
