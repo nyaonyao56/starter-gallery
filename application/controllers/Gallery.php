6e5cfbc47b83b263d34c8f5c7f405250c5f40f5f
nyaonyao56 <stanleychu56@gmail.com>
2017-01-29 17:05:14 -0800

    Added database functionality

---
+++ application/controllers/Gallery.php
@@ -0,0 +1,32 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gallery extends Application
+{
+	public function index()
+	{
+                $pix = $this->images->all();
+                foreach($pix as $picture){  
+                    $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+                }
+                //prime the table class
+		$this->load->library('table');
+                $parms =array(
+                    'table_open' => '<table class="gallery">',
+                    'cell_start' => '<td class="oneimage">',
+                    'cell_alt_start' => '<td class="oneimage">'
+                );
+                $this->table->set_template($parms);
+                
+                
+                $rows =$this->table->make_columns($cells, 3);
+                $this->data['thetable'] = $this->table->generate($rows);
+                
+                $this->data['pagebody'] = 'gallery'; 
+                //$this->load->view('gallery');
+                $this->render();
+                    
+	}
+
+}
